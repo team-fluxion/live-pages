27bfbd4483451df8df2886611c315b2e1ebcee48
myTerminal <team.terminal@gmail.com>
2020-09-16 12:07:59 -0500

    remove usage of internal name in manifest.json

--- webpack.prod.js
+++ webpack.prod.js
@@ -17,7 +17,7 @@ const copy = new CopyWebpackPlugin([
         from: `${templatesDir}/manifest.json`,
         transform: (content, path) =>
             content.toString()
-            .replace(/#short-name#/g, `${config.appName}`)
+            .replace(/#short-name#/g, `${config.friendlyName}`)
             .replace(/#long-name#/g, `${config.friendlyName}: ${config.description}`)
             .replace(/#static-path#/g, `${config.staticPath}`)
             .replace(/#background-color#/g, `${config.colors.backgroundColor}`)
