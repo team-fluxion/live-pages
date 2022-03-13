1c9177a58c76d3d07a4450de9498c803ecce7dc5
myTerminal <team.terminal@gmail.com>
2022-03-13 01:35:13 -0600

    fix handling of query strings on client

--- router/client.js
+++ router/client.js
@@ -206,8 +206,8 @@ const getVerticalDirectionForNavigation = (
 // Function to handle route changes on client
 const handleRoute = ({ state }, horizontalDirection = false) => {
     // Retrieve path variables
-    const { location: { pathname } } = document;
-    const interceptedPath = pathname.slice(0, 1) !== '/' ? `/${pathname}` : pathname;
+    const { location: { pathname, search } } = document;
+    const interceptedPath = pathname.slice(0, 1) !== '/' ? `/${pathname}${search}` : `${pathname}${search}`;
 
     // Find matching route, determine vertical direction
     const firstMatchingRoute = findChildRoute('/', appConfig.routes, interceptedPath);
